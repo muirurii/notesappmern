a91a13fcf8de48315192f4fc9377dbc84de8195a
muirurii <pnkanuna@gmail.com>
2022-06-06 18:19:10 +0300

    first commit

---
+++ model/postModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    title: {
+        type: String
+    },
+    body: {
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Post', PostSchema);
